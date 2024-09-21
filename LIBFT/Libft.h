376e9f5bc2a18f00b8f6244c5257d02fe95483d6
César Nieto Silva <148909056+LordC666@users.noreply.github.com>
2024-09-21 16:29:44 +0200

    Add files via upload

--- LIBFT/Libft.h
+++ LIBFT/Libft.h
@@ -5,19 +5,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int		ft_isalnum(char *str); 
-int		ft_isalpha(char *str);
-int		ft_isascii(char *str);
-int		ft_isdigit(char *str);
-int		ft_isprint(char *str);
+int		ft_isalnum(int c); 
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
-int		ft_strlen(char *str);
+int		ft_strlen(const char *str);
 int		ft_strncmp(char *s1, char *s2, unsigned int n);
-char	*ft_tolower(char *str);
-char	*ft_toupper(char *str);
+char	*ft_tolower(int c);
+char	*ft_toupper(int c);
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 char	*ft_strchr(const char *str, int c);
@@ -31,6 +31,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t number, size_t size);
-char *ft_strdup(const char *str);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 
 # endif
